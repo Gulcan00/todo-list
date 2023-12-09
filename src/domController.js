0d7fa662573e1899a5cf39cf6aff611c159c6256
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2023-12-09 19:17:47 +0200

    Add new task form and functionality

--- src/domController.js
+++ src/domController.js
@@ -1,5 +1,38 @@
+import projectController from "./projectController";
+import { createTodo } from "./todo";
+
 function showNewTodoForm() {
     
 
 
+}
+
+function displayTask() {
+
+}
+
+
+export default function domController() {
+    const projects = projectController();
+    const newTaskForm = document.getElementById('new-task');
+
+    newTaskForm.addEventListener('submit', (e) => {
+        if (newTaskForm.checkValidity()) {
+            e.preventDefault();
+            const formData = new FormData(newTaskForm);
+            const title = formData.get('task-name');
+            const description = formData.get('description');
+            const dueDate = formData.get('due-date');
+
+            const task = createTodo({title, description, dueDate, priority: "low"});
+            projects.getProjectByName('allTasks').addTodo(task);
+            
+        }
+
+    });
+
+    /**
+     * function to update page display tasks of the current active project
+     * activeProject.forEach(todo => displayTask())
+     */
 }
--- src/projectController.js
+++ src/projectController.js
@@ -1,17 +1,22 @@
 import { createProject } from "./project";
 
-export function projectController() {
-    let projects = [];
+export default function projectController() {
+    let projects = [createProject('allTasks')];
 
     const addProject = title => projects.push(createProject(title));
 
-    const deleteProject = title => projects = projects.filter(project => project.title !== title);
+    const deleteProject = title => {
+        projects = projects.filter(project => project.title !== title);
+    }
 
     const getProjects = () => projects;
+
+    const getProjectByName = (title) => projects.find(project => project.title === title);
 
     return {
         addProject,
         deleteProject,
-        getProjects
+        getProjects,
+        getProjectByName
     }
 }

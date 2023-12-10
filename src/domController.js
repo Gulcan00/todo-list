977fdaa51b94b14e0f3d913281cba8fbfb1aee3b
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2023-12-10 16:03:27 +0200

    Refactor task display and add priority field to new task form

--- src/domController.js
+++ src/domController.js
@@ -1,13 +1,24 @@
 import projectController from "./projectController";
 import createTodo from "./todo";
 
-function showNewTodoForm() {}
 
 function displayTask(task) {
   const container = document.createElement("div");
   container.classList.add("task");
 
-  const { title, description, dueDate } = task;
+  const { title, description, dueDate, priority } = task;
+
+  const titleDiv = document.createTextNode(title);
+  container.appendChild(titleDiv);
+
+  const descriptionDiv = document.createTextNode(description);
+  container.appendChild(descriptionDiv);
+
+  const dueDateDiv = document.createTextNode(dueDate);
+  container.appendChild(dueDateDiv);
+
+  const priorityDiv = document.createTextNode(priority);
+  container.appendChild(priorityDiv);
 
   return container;
 }
@@ -16,9 +27,12 @@ export default function domController() {
   const projects = projectController();
   const newTaskForm = document.getElementById("new-task");
   const tasksDiv = document.getElementById("tasks");
+  const addBtn = document.querySelector('.sidebar :first-child');
+  const cancelBtn = document.querySelector('.cancel');
 
   function updateScreen() {
     // based on current active project
+    tasksDiv.innerHTML = null;
     projects
       .getProjectByName("allTasks")
       .getTodos()
@@ -35,15 +49,20 @@ export default function domController() {
       const title = formData.get("task-name");
       const description = formData.get("description");
       const dueDate = formData.get("due-date");
+      const priority = formData.get("priority");
 
-      const task = createTodo({ title, description, dueDate, priority: "low" });
+      const task = createTodo({ title, description, dueDate, priority });
       projects.getProjectByName("allTasks").addTodo(task);
       updateScreen();
+
+      newTaskForm.reset();
     }
   });
 
-  /**
-   * function to update page display tasks of the current active project
-   * activeProject.forEach(todo => displayTask())
-   */
+  function toggleVisibility() {
+    newTaskForm.style.display = (newTaskForm.style.display === "none" || newTaskForm.style.display === "") ? "flex" : "none";
+  }
+
+  addBtn.addEventListener("click", toggleVisibility);
+  cancelBtn.addEventListener("click", toggleVisibility);
 }

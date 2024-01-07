6a01a7eb3282e5fc20f10a89ff3777c81edcd9d3
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2024-01-07 12:28:59 +0200

    Run build

--- webpack.common.js
+++ webpack.common.js
@@ -23,7 +23,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
-      favicon: "./images/task.png",
+      favicon: "./src/images/task.png",
     }),
   ],
 };
